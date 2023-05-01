f6c537f3f91b36740757d24d5b1c6e289ed65fd1
baddiescode <designerjunkiegfx@gmail.com>
2023-04-30 20:18:22 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://kaminahoakes:kaminahoakes@personal-express-app.qxowqa8.mongodb.net/shopping?retryWrites=true&w=majority', 
+    'dbName': 'shopping'
+};
